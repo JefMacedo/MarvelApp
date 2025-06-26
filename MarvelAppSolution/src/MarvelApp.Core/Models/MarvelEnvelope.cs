4534f1811948ac4ff4a3f0a467d39c92012a434c
Jeferson Macedo <jhef.salles@gmail.com>
2025-06-25 23:43:08 -0300

    Add initial project structure with core, infrastructure, and API components

---
+++ MarvelAppSolution/src/MarvelApp.Core/Models/MarvelEnvelope.cs
@@ -0,0 +1,15 @@
+﻿namespace MarvelApp.Core.Models;
+
+public class MarvelEnvelope<T>
+{
+    public MarvelDataContainer<T> Data { get; set; } = default!;
+}
+
+public class MarvelDataContainer<T>
+{
+    public int Offset { get; set; }
+    public int Limit { get; set; }
+    public int Total { get; set; }
+    public int Count { get; set; }
+    public List<T> Results { get; set; } = new();
+}
